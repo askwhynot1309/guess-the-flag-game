54913f594640bb109e30e472de1944924ecb5829
AskWhyNot <tommarvoloriddle1975@gmail.com>
2025-04-21 21:42:35 +0700

    fix json leak

--- guess-the-flag-game/Assets/CommandLineReader.cs
+++ guess-the-flag-game/Assets/CommandLineReader.cs
@@ -30,7 +30,7 @@ public class CommandLineReader : MonoBehaviour
                 if (string.IsNullOrEmpty(UserId)) UserId = "3cd94c6a-7bbe-4b08-bd4e-961ec9e41930";
                 if (string.IsNullOrEmpty(FloorId)) FloorId = "1";
                 if (string.IsNullOrEmpty(GameId)) GameId = "b834ad5204834367a06be32cb4baf818    ";
-                if (string.IsNullOrEmpty(AccessToken)) AccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJDdXN0b21lciIsInVzZXJJZCI6IjNjZDk0YzZhLTdiYmUtNGIwOC1iZDRlLTk2MWVjOWU0MTkzMCIsImZ1bGxuYW1lIjoicXV5ZW4iLCJlbWFpbCI6InVzZXJAZ21haWwuY29tIiwicm9sZSI6IkN1c3RvbWVyIiwiYXZhdGFyVXJsIjoiaHR0cHM6Ly9ncmF0aXNvZ3JhcGh5LmNvbS93cC1jb250ZW50L3VwbG9hZHMvMjAyNS8wMS9ncmF0aXNvZ3JhcGh5LWRvZy12YWNhdGlvbi04MDB4NTI1LmpwZyIsImV4cCI6MTc0NTI0NjU2OCwiaXNzIjoiSW50ZXJhY3RpdmVGbG9vciIsImF1ZCI6IkludGVyYWN0aXZlRmxvb3IifQ.kWUF4TQgWv6cUJEk48GV056XWWIyI0hJEDGHQ_8bN88";
+                if (string.IsNullOrEmpty(AccessToken)) AccessToken = "abc";
         #endif
 
         Debug.Log($"[Startup Args] userId: {UserId}, floorId: {FloorId}, gameId: {GameId}, accessToken: {AccessToken}");
